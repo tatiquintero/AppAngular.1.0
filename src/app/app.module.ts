1755af6bb67b52d27419d3f49c3ab4cd3c487faa
thunderthard <hars1302@gmail.com>
2022-11-21 14:04:34 -0500

    arreglo identificacion componente mal creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IdentificacionComponent } from './modulo/seguridad/identificacion/identificacion.component';
 import { BarraNavegacionComponent } from './plantillas/barra-navegacion/barra-navegacion.component';
 import { PiepaginaComponent } from './plantillas/piepagina/piepagina.component';
 import { InicioComponent } from './plantillas/inicio/inicio.component';
 import { ErrorComponent } from './plantillas/error/error.component';
 import {HttpClientModule} from '@angular/common/http'
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IdentificacionComponent,
     BarraNavegacionComponent,
     PiepaginaComponent,
     InicioComponent,
     ErrorComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     HttpClientModule
   ],
   providers: [],
